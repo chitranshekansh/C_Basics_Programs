5a809963cf36b650bfab0f9c0f1af80cc8ba4847
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2022-12-13 14:50:36 +0530

    Add files via upload

---
+++ CALCULAT.C
@@ -0,0 +1,31 @@
+//Arthimetic Calculator
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char x;
+float a,b,res;
+clrscr();
+printf("Enter value 1:");
+scanf("%f",&a);
+printf("Enter value 2:");
+scanf("%f",&b);
+printf(" + for Addition\n - for Sutraction\n * for Product\n \\ for division\nPRESS:");
+fflush(stdin);
+scanf("%c",&x);
+switch(x)
+{
+case '+': res=a+b;
+	  break;
+case '-': res=a-b;
+	  break;
+case '*': res=a*b;
+	  break;
+case '/': res=a/b;
+	  break;
+default : printf("Wrong Choice");
+}
+if(x=='+'||x=='-'||x=='*'||x=='/')
+printf("RESULT:%.2f",res);
+getch();
+}
