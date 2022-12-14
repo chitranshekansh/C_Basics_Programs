e193fd28b218e707fe1a9ce010e7496c6ac2e301
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2022-12-14 23:37:46 +0530

    Add files via upload

---
+++ FACTORIA.C
@@ -0,0 +1,16 @@
+//Program to find factorial
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long int i,fact=1,n;
+clrscr();
+printf("Enter Range:");
+scanf("%ld",&n);
+for(i=1;i<=n;i++)
+{
+fact*=i;
+}
+printf("The factorial value of %ld is:%ld",n,fact);
+getch();
+}
