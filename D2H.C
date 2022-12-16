ab9c1602a7752bbbd520affec256294009675fa2
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2022-12-16 21:33:11 +0530

    Add files via upload

---
+++ D2H.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("Enter decimal value:");
+scanf("%d",&a);
+printf("Hexadecimal Value:%x",a);
+printf("\nEnter hexadecimal value:");
+scanf("%x",&b);
+printf("Decimal Value:%d",b);
+getch();
+}
---
+++ D2O.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("Enter Decimal Value:");
+scanf("%d",&a);
+printf("Octal:%o",a);
+printf("\nEnter Octal Value:");
+scanf("%o",&b);
+printf("Decimal:%d",b);
+getch();
+}
