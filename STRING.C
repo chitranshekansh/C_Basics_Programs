84003621bc1d0ad8dcfd052d012980869d47cff8
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-01-17 23:00:37 +0530

    Add files via upload

---
+++ STRING.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char sname[20],fname[20];
+clrscr();
+printf("Enter your name:");
+gets(sname);
+printf("Enter your father name:");
+gets(fname);
+getche();
+}
